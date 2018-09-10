b4cf80dcf521ac811633cb16108b7fc723383b43
Dominic Tarr <dominic.tarr@gmail.com>
2018-09-10 12:45:34 -0700

    append logic

--- append.js
+++ append.js
@@ -13,10 +13,23 @@ exports.append = function (state, buffer) {
   var last = state.buffers[state.buffers.length-1]
   if(!last) throw new Error('no last buffer')
   var start = state.offset%state.block
+  if(start + buffer.length + 4 > state.block - 6) {
+    console.log(start, state.offset, buffer.length)
+    last.writeUInt16LE(state.block-1, start)
+    last.writeUInt32LE(start, state.block-4)
+    state.offset = ~~(start/state.block) + state.block
+    start = 0
+    state.buffers.push(last = Buffer.alloc(state.block))
+  }
   last.writeUInt16LE(buffer.length, start)
   buffer.copy(last, start+2)
   last.writeUInt16LE(buffer.length, start+2+buffer.length)
   state.offset += 4+buffer.length
+
   return state
 }
+
+
+
+
 
