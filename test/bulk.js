23e6c641cf72e10534a6bb7bd9ad1a6e98e13930
Dominic Tarr <dominic.tarr@gmail.com>
2018-09-13 20:17:57 +1200

    bulk test needs fs!

--- test/bulk.js
+++ test/bulk.js
@@ -1,6 +1,7 @@
 var crypto = require('crypto')
 var tape = require('tape')
 var RAF = require('../')
+var fs = require('fs')
 function random () {
   return crypto.randomBytes(100 + ~~(Math.random()*1000))
 }
@@ -12,21 +13,20 @@ var raf = RAF(filename, {block: 64*1024})
 var a = []
 
 tape('insert random data', function (t) {
-  for(var i = 0; i < 1; i++) {
+  for(var i = 0; i < 2000; i++) {
     var b = random()
     a.push(b)
     raf.append(b, function () {})
   }
   raf.onDrain(t.end)
 })
-return
+
 function collect(cb) {
   var ary = []
   return {
     write: ary.push.bind(ary),
-    end: function () {
-      t.deepEqual(ary, a)
-      t.end()
+    end: function (err) {
+      cb(err, ary)
     }
   }
 }
@@ -34,6 +34,7 @@ function collect(cb) {
 tape('stream', function (t) {
   raf.stream().pipe(collect(function (err, ary) {
     t.deepEqual(ary, a)
+    t.end()
   }))
 })
 
@@ -45,10 +46,7 @@ tape('stream, reload', function (t) {
     t.deepEqual(ary, a)
     t.end()
   }))
-
 })
-
-
 
 
 
