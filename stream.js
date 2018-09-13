439f0739041569b0b6dae73dbc307dcab1e74ea4
Dominic Tarr <dominic.tarr@gmail.com>
2018-09-13 18:39:28 +1200

    implement reverse

--- stream.js
+++ stream.js
@@ -15,34 +15,30 @@ function Stream (blocks, opts) {
 
 Stream.prototype._ready = function () {
   if(this.reverse) {
-    if(!ltgt.upperBoundInclusive(opts))
     this.cursor = this.start = ltgt.upperBound(this.opts, this.blocks.length)
     this.end = ltgt.lowerBound(this.opts, 0)
-    /*
-    return this.blocks.getPrevious(self.start, function (err, buffer, start, length) {
-      self.cursor = start - 2
-      self.resume() //start reading
-    })
-    else
-      self.cursor = start
-    */
   }
   else {
     this.cursor = this.start = ltgt.lowerBound(this.opts, 0)
     this.end = ltgt.upperBound(this.opts, this.blocks.length)
-    var self = this
-    this.blocks.getBlock(~~(self.start/self.blocks.block), function (err, buffer) {
-      self._buffer = buffer
-      self.resume()
-    })
   }
+  
+  var self = this
+  this.blocks.getBlock(~~(self.start/self.blocks.block), function (err, buffer) {
+    self._buffer = buffer
+    console.log(buffer)
+    self.resume()
+  })
 }
 
 Stream.prototype._next = function () {
-  if(!this._buffer || this.cursor >= this.blocks.length) return
+  if(!this._buffer || this.start === -1 || this.isAtEnd()) return
   if(!this.reverse) {
     var result = frame.getRecord(this.blocks.block, this._buffer, this.cursor%this.blocks.block)
-    if(!result) {
+    if(result) {
+      this.cursor += result.length + 4
+      return result
+    } else {
       //move to start of next block
       this.cursor = (this.cursor - this.cursor%this.blocks.block)+this.blocks.block
       if(this.cursor < this.blocks.length) {
@@ -53,13 +49,30 @@ Stream.prototype._next = function () {
         })
       }
     }
-    else {
-      this.cursor += result.length + 4
+  }
+  else {
+    if(this.cursor == 0) {
+      this.cursor --
+    }
+    else if(this.cursor % this.blocks.block) {
+      var result = frame.getPreviousRecord(this.blocks.block, this._buffer, this.cursor%this.blocks.block)
+      this.cursor -= (result.length+4)
       return result
     }
+    else {
+      var self = this
+      this.blocks.getBlock(~~(this.cursor/this.blocks.block)-1, function (err, buffer) {
+        self._buffer = buffer
+        //point at padding in block
+        self.cursor = buffer.readUInt32LE(this.blocks.block-4)
+        self.resume()
+      })
+    }
   }
-  else
-    throw new Error('not yet implemented')
+}
+
+Stream.prototype.isAtEnd = function () {
+  return this.reverse ? this.cursor < 0 : this.cursor >= this.blocks.length
 }
 
 Stream.prototype.resume = function () {
@@ -67,7 +80,7 @@ Stream.prototype.resume = function () {
   while(this.sink && !this.sink.paused && !this.ended) {
     var result = this._next()
     if(result && result.length) this.sink.write(this._buffer.slice(result.start, result.start+result.length))
-    else if(!this.live && (result ? result.length == 0 : this.cursor >= this.blocks.length)) {
+    else if(!this.live && (result ? result.length == 0 : this.isAtEnd())) {
       this.ended = true
       this.sink.end()
       return
@@ -86,6 +99,7 @@ Stream.prototype.abort = function () {
 }
 
 Stream.prototype.pipe = require('push-stream/pipe')
+
 
 
 
