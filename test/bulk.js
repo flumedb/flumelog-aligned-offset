475c2231865675c5ee6b0f065ba59940483f12ec
Dominic Tarr <dominic.tarr@gmail.com>
2018-09-14 22:08:32 +1200

    test ltgt ranges

--- test/bulk.js
+++ test/bulk.js
@@ -56,3 +56,56 @@ tape('stream, reverse', function (t) {
   }))
 })
 
+tape('seqs', function (t) {
+  raf.stream({}).pipe(collect(function (err, ary) {
+    t.equal(ary[0].seq, 0)
+    t.deepEqual(ary.map(function (e) { return e.value }), a)
+      var i = ~~(ary.length*Math.random())
+      console.log(ary[i])
+      raf.stream({gte: ary[i].seq}).pipe(collect(function (err, _ary) {
+        if(err) throw err
+
+        t.equal(_ary.length, a.length-i)
+        t.equal(_ary[0].seq, ary[i].seq)
+        t.deepEqual(_ary, ary.slice(i))
+        raf.stream({gt: ary[i].seq}).pipe(collect(function (err, _ary) {
+
+          t.equal(_ary.length, a.length-(i+1))
+          t.equal(_ary[0].seq, ary[i+1].seq)
+//          t.deepEqual(_ary, ary.slice(i+1))
+
+          t.end()
+        }))
+    }))
+  }))
+})
+
+tape('seqs', function (t) {
+  raf.stream({}).pipe(collect(function (err, ary) {
+    t.equal(ary[0].seq, 0)
+    t.deepEqual(ary.map(function (e) { return e.value }), a)
+    var i = 20//~~(ary.length*Math.random())
+    raf.stream({lte: ary[i].seq}).pipe(collect(function (err, _ary) {
+      if(err) throw err
+
+      t.equal(_ary.length, i+1)
+      t.equal(_ary[0].seq, ary[0].seq)
+      t.deepEqual(_ary, ary.slice(0, i+1))
+
+      raf.stream({lt: ary[i].seq}).pipe(collect(function (err, _ary) {
+//        console.log(_ary.slice(0, i), i, ary[i].seq)
+        t.equal(_ary.length, i)
+        console.log(_ary[i-1].seq, ary[i].seq)
+        t.ok(_ary[_ary.length-1].seq < ary[i].seq)
+        t.equal(_ary[0].seq, 0)
+        t.equal(_ary[0].seq, ary[0].seq)
+        t.ok(_ary[i-1].seq < ary[i].seq)
+        t.equal(_ary[0].seq, ary[0].seq)
+        t.end()
+      }))
+    }))
+  }))
+})
+
+
+
