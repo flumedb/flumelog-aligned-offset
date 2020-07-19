61eaeabbfd3248efdb05005a4e869c7618d3d427
Anders Rune Jensen <anders@iola.dk>
2020-07-19 21:37:02 +0200

    Get tests closer to working again

--- test/flumeview-read.js
+++ test/flumeview-read.js
@@ -1,43 +1,23 @@
-
 var Flume = require('flumedb')
 var Index = require('flumeview-level')
 var codec = require('flumecodec')
+var fs = require('fs')
 
 var create = require('..')
 
 var toCompat = require('../compat')
 
 require('test-flumeview-index/read')(function (filename, seed) {
-  var raf = create(filename+'/aligned.log', {
+  if (!fs.existsSync(filename))
+    fs.mkdirSync(filename)
+
+  var raf = create(filename + '/aligned.log', {
     blockSize: 1024*64,
     codec: require('flumecodec/json')
   })
 
   return Flume(toCompat(raf))
     .use('index', Index(1, function (e) {
-      console.log(e)
       return [e.key]
     }))
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- test/flumeview.js
+++ test/flumeview.js
@@ -1,32 +1,26 @@
-
 var Flume = require('flumedb')
-var Log = require('flumelog-offset')
 var Index = require('flumeview-level')
 var codec = require('flumecodec')
+var fs = require('fs')
 
 var create = require('..')
 
 var toCompat = require('../compat')
 
 require('test-flumeview-index')(function (filename, seed) {
-  var raf = create(filename+'/aligned.log', {
+  if (!fs.existsSync(filename))
+    fs.mkdirSync(filename)
+
+  var raf = create(filename + '/aligned.log', {
     blockSize: 1024*64,
     codec: require('flumecodec/json')
   })
 
   return Flume(toCompat(raf))
     .use('index', Index(1, function (e) {
-      console.log(e)
       return [e.key]
     }))
 })
-
-
-
-
-
-
-
 
 
 
